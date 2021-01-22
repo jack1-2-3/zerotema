042d79ba552db2e1c130abf5fc3bd2010d0f6047
jack <jack.programming123@gmail.com>
2021-01-22 16:19:03 +0900

    フォルダ整理&cssをscssにコンバート&ローディング画面設置

---
+++ assets/js/loader.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function () {
+    const wrapper = document.querySelector('.wrapper');
+    const loader = document.querySelector('#loader');
+    setTimeout(() => {
+        loader.style.display = 'none';
+        wrapper.style.display = 'block';
+    }, 3000);
+});
