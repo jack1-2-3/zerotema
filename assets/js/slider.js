a6f203b26c746acddb22a8c337672ba8178a9602
jack <jack.programming123@gmail.com>
2021-01-24 14:57:01 +0900

    slider実装

---
+++ assets/js/slider.js
@@ -0,0 +1,15 @@
+const mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    centeredSlides: true,
+    slidesPerView: 1,
+    autoplay: true,
+    speed: 1000,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+mySwiper.autoplay.start();
