3f312212af16d5ceda1c6006c55e799d8326bf15
jack <jack.programming123@gmail.com>
2021-01-28 13:03:10 +0900

    修正

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,8 +1,10 @@
-window.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const wrapper = document.querySelector('.wrapper');
     const loader = document.querySelector('#loader');
     setTimeout(() => {
         loader.style.display = 'none';
         wrapper.style.display = 'block';
     }, 3000);
+
+    
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
-'use strict';
 // header
-const hamburger = document.querySelector('.hamburger');
-const mobMenu = document.querySelector('.header_inner');
-hamburger.addEventListener('click', function () {
-    mobMenu.classList.toggle('open');
-});
-
-const headerLinks = document.querySelectorAll('.js_header-link');
-headerLinks.forEach((link) => {
-    link.addEventListener('click', function () {
-        mobMenu.classList.remove('open');
-    });
-});
 
 // scroll observer
 window.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const mobMenu = document.querySelector('.header_inner');
+    hamburger.addEventListener('click', function () {
+        mobMenu.classList.toggle('open');
+    });
+
+    const headerLinks = document.querySelectorAll('.js_header-link');
+    headerLinks.forEach((link) => {
+        link.addEventListener('click', function () {
+            mobMenu.classList.remove('open');
+        });
+    });
     new ScrollObserver('.js_scroll', function (el, inview) {
         if (inview) {
             el.classList.add('js_scroll-active');
